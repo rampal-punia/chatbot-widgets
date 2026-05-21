55d94ff024105684cd13e8ca003240ce86651784
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-21 17:11:57 +0000

    Add: eslint.config.js
    ✨ Auto-committed on 2026-05-21 17:11:57

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    files: ['packages/react/src/index.js'],
+    languageOptions: {
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        exports: 'writable',
+        require: 'readonly',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-undef': 'off',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'wordpress/',
+      'local_folder/',
+      '*.config.js',
+    ],
+  },
+);
