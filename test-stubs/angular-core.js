83d720692c52c785327cf3a42f4ae5bf8ee859be
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-21 18:04:27 +0000

    Update: test-stubs/angular-core.js
    ✨ Auto-committed on 2026-05-21 18:04:27

--- test-stubs/angular-core.js
+++ test-stubs/angular-core.js
@@ -1,6 +1,6 @@
 // Stub for @angular/core -- used during testing only
-export function Component(opts) {
-  return function (cls) {};
+export function Component(_opts) {
+  return function (_cls) {};
 }
 export function EventEmitter() {
   this.handlers = [];
@@ -13,13 +13,13 @@ EventEmitter.prototype.emit = function (val) {
 EventEmitter.prototype.subscribe = function (handler) {
   this.handlers.push(handler);
 };
-export function Inject(token) {
-  return function (target, key, index) {};
+export function Inject(_token) {
+  return function (_target, _key, _index) {};
 }
 export function Input() {
-  return function (target, key) {};
+  return function (_target, _key) {};
 }
 export function Output() {
-  return function (target, key) {};
+  return function (_target, _key) {};
 }
 export var PLATFORM_ID = 'platformId';
