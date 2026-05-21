0873a98f5b2484a6e2efbc4d440519940308e8d6
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-21 17:59:08 +0000

    Add: packages/react/src/global.d.ts
    ✨ Auto-committed on 2026-05-21 17:59:08

---
+++ packages/react/src/global.d.ts
@@ -0,0 +1,11 @@
+interface Window {
+  AparsoftChatbot: {
+    isReady?: boolean;
+    state?: string;
+    open?: () => void;
+    close?: () => void;
+    toggle?: () => void;
+    sendMessage?: (message: string) => void;
+    destroy?: () => void;
+  } | null | undefined;
+}
