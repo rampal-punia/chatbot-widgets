a3c850ce26034451a9843a2d6bf5bd2178224a5a
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-21 18:04:27 +0000

    Update: packages/angular/src/global.d.ts
    ✨ Auto-committed on 2026-05-21 18:04:27

--- packages/angular/src/global.d.ts
+++ packages/angular/src/global.d.ts
@@ -1,11 +1,14 @@
 interface Window {
-  AparsoftChatbot: {
-    isReady?: boolean;
-    state?: string;
-    open?: () => void;
-    close?: () => void;
-    toggle?: () => void;
-    sendMessage?: (message: string) => void;
-    destroy?: () => void;
-  } | null | undefined;
+  AparsoftChatbot:
+    | {
+        isReady?: boolean;
+        state?: string;
+        open?: () => void;
+        close?: () => void;
+        toggle?: () => void;
+        sendMessage?: (message: string) => void;
+        destroy?: () => void;
+      }
+    | null
+    | undefined;
 }
